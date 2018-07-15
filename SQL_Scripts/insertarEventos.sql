f23bc3aec62bf05c5dde220f5a26f05fe85847bf
rrondosa <1017.reyna@gmail.com>
2018-07-15 00:13:33 -0300

    first commit

---
+++ SQL_Scripts/insertarEventos.sql
@@ -0,0 +1,6 @@
+USE tp;
+
+insert into Evento (id, correo, descripcion, facebook, fecha, horaFin, horaInicio, imagen1, imagen2, imagen3, instagram, mostrar, nombre, telefono, twitter) 
+values  (1, 'evento01@eventos.com.ar', 'Aca va la descripcion del Evento 01', 'evento01', '2018-08-01', '0:00', '19:00', 'evento06.jpg', 'evento07.jpg', 'evento01.jpg', 'evento 01', 'si', 'Evento 01', '4338-5800', '#evento01'), 
+		(2, 'evento02@eventos.com.ar', 'Aca va la descripcion del Evento 02', 'evento02', '2018-08-01', '0:00', '19:00', 'evento02.jpg', 'evento03.jpg', 'evento04.jpg', 'evento 02', 'si', 'Evento 02', '4338-5800', '#evento02'), 
+		(3, 'evento03@eventos.com.ar', 'Aca va la descripcion del Evento 03', 'evento03', '2018-08-01', '0:00', '19:00', 'evento04.jpg', 'evento01.jpg', 'evento06.jpg', 'evento 03', 'si', 'Evento 03', '4338-5800', '#evento03') 
---
+++ SQL_Scripts/insertarUsuario.sql
@@ -0,0 +1,3 @@
+USE tp;
+
+INSERT INTO `tp`.`usuario` (`id`, `email`, `nombre`, `password`, `rol`) VALUES ('1', 'admin@admin.com', 'admin', '1234', 'admin');
